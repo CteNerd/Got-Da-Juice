bf37d75cf1ec542939cb073b56019908e22e4732
Roderick C Tomlin II <34181724+CteNerd@users.noreply.github.com>
2024-09-02 13:03:13 -0500

    header mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import Logo from '../../assets/images/Website_Logo.png'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className="header">
-            <nav className="nav">
-                <div className="dropdown">
-                    <a className="menu-text" href='/'>Home</a>
-                </div>
-                <div className="dropdown">
-                    <span className="menu-text">Menu</span>
-                    <div className="dropdown-content">
-                        <a href="#menu1">All</a>
-                        <a href="#menu2">Menu 2</a>
-                    </div>
-                </div>
+            <div className="logo-container">
                 <img src={Logo} alt="Logo" className="logo" />
-                <div className="dropdown">
-                    <a className="menu-text" href='https://www.toasttab.com/got-da-juice-309-pacific-ave/rewardsLookup'>Juice Club</a>
-                </div>
-                <div className="dropdown">
-                    <span href="#more">More</span>
-                    <div className="dropdown-content">
-                        <a href="#club1">Blog</a>
-                        <a href="#club2">About Us</a>
-                        <a href="#club2">Contact Us</a>
-                    </div>
-                </div>
+            </div>
+            <div className={`hamburger-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+            <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#juice-club">Juice Club</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
             </nav>
+            <div className={`overlay ${isMenuOpen ? 'overlay-active' : ''}`} onClick={toggleMenu}></div>
         </header>
     );
 };
