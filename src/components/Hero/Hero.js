b2d8f2c5b2a82ff7cdb8ee6d469b368a73fec32f
Roderick C Tomlin II <34181724+CteNerd@users.noreply.github.com>
2024-09-02 12:47:10 -0500

    updating hero. need hero img

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,9 @@ import './Hero.css';
 const Hero = ({heroImg, heroImgTxt = true}) => {
 
     return (
-        <section className={heroImg.includes('Green Juice Blended') ? 'hero-green-juice-blended' : 'hero-juice-set'}>
-            <div className="hero-content">
-                <h2 style={heroImgTxt ? {display: ''} : {display: 'none'}}>More than a juice bar. A vibrant Jersey City experience.</h2>
+        <section className={'hero'}>
+            <div className="hero-text">
+                <h1 style={heroImgTxt ? {display: ''} : {display: 'none'}}>More than a juice bar. A vibrant Jersey City experience.</h1>
                 <a style={heroImgTxt ? {display: ''} : {display: 'none'}} href="tel:+12012738227" className="phone-number">(201) 273-8227</a>
                 <button className="cta-button">Order Now</button>
             </div>
