1103d54d97ab3f94ce95741e7c84d6851879e3d2
Roderick C Tomlin II <34181724+CteNerd@users.noreply.github.com>
2024-09-05 23:33:12 -0500

    Update Hero.js
    link to menu

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = ({heroImgTxt = true, displayCtaBtn = false, className, displayHeroT
             <div className="hero-text" style={displayHeroTxt ? {display: ''} : {display: 'none'}}>
                 <h1 style={heroImgTxt ? {display: ''} : {display: 'none'}}>More than a juice bar. A vibrant Jersey City experience.</h1>
                 <a style={heroImgTxt ? {display: ''} : {display: 'none'}} href="tel:+12012738227" className="phone-number">(201) 273-8227</a>
-                <button style={displayCtaBtn ? {display: ''} : {display: 'none'}} className="cta-button">Order Now</button>
+                <a href='/menu'><button style={displayCtaBtn ? {display: ''} : {display: 'none'}} className="cta-button">Order Now</button></a>
             </div>
         </section>
     );
